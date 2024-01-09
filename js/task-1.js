ee73e4fc0f4236cfdb671e97d76ef9d6fdf7921a
Artem0202 <artyomkov.19@gmail.com>
2024-01-09 23:38:46 +0200

    task-3 and -4 and update task-1

--- js/task-1.js
+++ js/task-1.js
@@ -2,9 +2,9 @@
 const categories = document.querySelectorAll(".item");
 console.log(`Nomber of categories: ${categories.length}`);
 
-categories.forEach((items) => {
+categories.forEach((items, index) => {
   const title = items.firstElementChild;
   console.log(`Category: ${title.textContent}`);
-  const elements = items.lastElementChild;
-  console.log(elements.length);
+  const elements = items.lastElementChild.children;
+  console.log(`Elements: ${elements.length}`);
 });
