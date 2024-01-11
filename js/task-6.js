493a138591dce38ed91606ef97d686ec15d26d05
Artem0202 <artyomkov.19@gmail.com>
2024-01-12 00:47:20 +0200

    Task-6

--- js/task-6.js
+++ js/task-6.js
@@ -4,3 +4,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const inputNumber = document.querySelector("input");
+const createBoxesButton = document.querySelector(".create");
+const destroyBoxesButton = document.querySelector(".destroy");
+const outputBoxes = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  if (amount > 0 && amount < 101) {
+    for (let i = 0; i < amount; i++) {
+      const lastItem = document.createElement("div");
+      lastItem.style.width = `${30 + i * 10}px`;
+      lastItem.style.height = `${30 + i * 10}px`;
+      lastItem.style.background = `${getRandomHexColor()}`;
+      outputBoxes.append(lastItem);
+    }
+  }
+}
+
+function destroyBoxes() {
+  outputBoxes.innerHTML = "";
+}
+
+createBoxesButton.addEventListener("click", () => {
+  outputBoxes.innerHTML = "";
+  createBoxes(inputNumber.value);
+  inputNumber.value = "";
+});
+
+destroyBoxesButton.addEventListener("click", destroyBoxes);
